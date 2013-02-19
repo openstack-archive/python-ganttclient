mikalstill/nova

libvirt: Use uuid for instance directory name
This is done in a backwards compatible manner to deal with instances
started under the old regieme where we used a less unique name.

Resolves bug 1023865.

Change-Id: I4c9b7b291a30a7f3c2c9305c8fa346dc582bee88

nova/tests/fake_libvirt_utils.py
@@ -18,6 +18,7 @@ import os
 import StringIO
 
 from nova.openstack.common import cfg
+from nova.virt.libvirt import utils as libvirtutils_orig
 
 
 CONF = cfg.CONF
@@ -141,6 +142,6 @@ def fetch_image(context, target, image_id, user_id, project_id):
     pass
 
 
-def get_instance_path(instance):
+def get_instance_path(instance, forceold=False):
     # TODO(mikal): we should really just call the real one here
-    return os.path.join(CONF.instances_path, instance['name'])
+    return libvirtutils_orig.get_instance_path(instance, forceold=forceold)

openstack-archive/python-ganttclient

nova/tests/fake_libvirt_utils.py
@@ -18,6 +18,7 @@ import os
 import StringIO
 
 from nova.openstack.common import cfg
+from nova.virt.libvirt import utils as libvirt_utils
 
 
 CONF = cfg.CONF
@@ -141,9 +142,8 @@ def fetch_image(context, target, image_id, user_id, project_id):
     pass
 
 
-def get_instance_path(instance):
-    # TODO(mikal): we should really just call the real one here
-    return os.path.join(CONF.instances_path, instance['name'])
+def get_instance_path(instance, forceold=False):
+    return libvirt_utils.get_instance_path(instance, forceold=forceold)
 
 
 def pick_disk_driver_name(is_block_dev=False):