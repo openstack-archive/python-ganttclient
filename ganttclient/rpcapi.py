95bc9c96931f49e4967c7847692edd6c783fee93
Swapnil Kulkarni <swapnilkulkarni2608@gmail.com>
2014-01-13 11:57:49 +0530

    Initial changes for ganttclient
    Updated .gitignore to replace nova with python-ganttclient.
    Updated HACKING.rst to remove nova related references.
    Updated README.rst, the developer notes page need to be created, kept
    in TODO.
    Updated nova references in .testr.conf and .coveragerc.
    Added tox.ini, setup.py, setup.cfg, requirements.txt,
    test-requirements.txt
    Updated initial code for failures.
    Added initial documentation for ganntclient
    Commented test_rpcapi
    
    Change-Id: If1b3cee69bc7904452efdedfa3488a86f07bf6b5

--- ganttclient/rpcapi.py
+++ ganttclient/rpcapi.py
@@ -32,62 +32,23 @@ rpcapi_opts = [
 CONF = cfg.CONF
 CONF.register_opts(rpcapi_opts)
 
-rpcapi_cap_opt = cfg.StrOpt('scheduler',
-        help='Set a version cap for messages sent to scheduler services')
+rpcapi_cap_opt = [
+    cfg.StrOpt('scheduler',
+               help='Set a version cap for messages sent to gantt services'),
+]
 CONF.register_opt(rpcapi_cap_opt, 'upgrade_levels')
 
 
 class SchedulerAPI(rpcclient.RpcProxy):
-    '''Client side of the scheduler rpc API.
+    '''Client side of the gantt rpc API.
 
     API version history:
 
         1.0 - Initial version.
-        1.1 - Changes to prep_resize():
-                - remove instance_uuid, add instance
-                - remove instance_type_id, add instance_type
-                - remove topic, it was unused
-        1.2 - Remove topic from run_instance, it was unused
-        1.3 - Remove instance_id, add instance to live_migration
-        1.4 - Remove update_db from prep_resize
-        1.5 - Add reservations argument to prep_resize()
-        1.6 - Remove reservations argument to run_instance()
-        1.7 - Add create_volume() method, remove topic from live_migration()
 
-        2.0 - Remove 1.x backwards compat
-        2.1 - Add image_id to create_volume()
-        2.2 - Remove reservations argument to create_volume()
-        2.3 - Remove create_volume()
-        2.4 - Change update_service_capabilities()
-                - accepts a list of capabilities
-        2.5 - Add get_backdoor_port()
-        2.6 - Add select_hosts()
-
-        ... Grizzly supports message version 2.6.  So, any changes to existing
-        methods in 2.x after that point should be done such that they can
-        handle the version_cap being set to 2.6.
-
-        2.7 - Add select_destinations()
-        2.8 - Deprecate prep_resize() -- JUST KIDDING.  It is still used
-              by the compute manager for retries.
-        2.9 - Added the legacy_bdm_in_spec parameter to run_instance()
-
-        ... Havana supports message version 2.9.  So, any changes to existing
-        methods in 2.x after that point should be done such that they can
-        handle the version_cap being set to 2.9.
-
-        ... - Deprecated live_migration() call, moved to conductor
     '''
 
-    #
-    # NOTE(russellb): This is the default minimum version that the server
-    # (manager) side must implement unless otherwise specified using a version
-    # argument to self.call()/cast()/etc. here.  It should be left as X.0 where
-    # X is the current major API version (1.0, 2.0, ...).  For more information
-    # about rpc API versioning, see the docs in
-    # openstack/common/rpc/dispatcher.py.
-    #
-    BASE_RPC_API_VERSION = '2.0'
+    BASE_API_VER = '1.0'
 
     VERSION_ALIASES = {
         'grizzly': '2.6',
@@ -98,19 +59,20 @@ class SchedulerAPI(rpcclient.RpcProxy):
         version_cap = self.VERSION_ALIASES.get(CONF.upgrade_levels.scheduler,
                                                CONF.upgrade_levels.scheduler)
         super(SchedulerAPI, self).__init__(topic=CONF.scheduler_topic,
-                default_version=self.BASE_RPC_API_VERSION,
-                version_cap=version_cap)
+                                           default_version=self.BASE_API_VER,
+                                           version_cap=version_cap)
         self.client = self.get_client()
 
     def select_destinations(self, ctxt, request_spec, filter_properties):
         cctxt = self.client.prepare(version='2.7')
         return cctxt.call(ctxt, 'select_destinations',
-            request_spec=request_spec, filter_properties=filter_properties)
+                          request_spec=request_spec,
+                          filter_properties=filter_properties)
 
     def run_instance(self, ctxt, request_spec, admin_password,
-            injected_files, requested_networks, is_first_time,
-            filter_properties, legacy_bdm_in_spec=True):
-        version = '2.0'
+                     injected_files, requested_networks, is_first_time,
+                     filter_properties, legacy_bdm_in_spec=True):
+        version = '1.0'
         msg_kwargs = {'request_spec': request_spec,
                       'admin_password': admin_password,
                       'injected_files': injected_files,
@@ -124,7 +86,7 @@ class SchedulerAPI(rpcclient.RpcProxy):
         return cctxt.cast(ctxt, 'run_instance', **msg_kwargs)
 
     def prep_resize(self, ctxt, instance, instance_type, image,
-            request_spec, filter_properties, reservations):
+                    request_spec, filter_properties, reservations):
         instance_p = jsonutils.to_primitive(instance)
         instance_type_p = jsonutils.to_primitive(instance_type)
         reservations_p = jsonutils.to_primitive(reservations)
